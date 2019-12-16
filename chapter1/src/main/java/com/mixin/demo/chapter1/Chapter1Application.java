ec4c4b9e06a95794dbba8c5515843ecba954aac8
niweiMac <niwei@yodoo.net.cn>
2019-12-16 14:58:33 +0800

    chapter1 add api

--- chapter1/src/main/java/com/mixin/demo/chapter1/Chapter1Application.java
+++ chapter1/src/main/java/com/mixin/demo/chapter1/Chapter1Application.java
@@ -1,8 +1,11 @@
 package com.mixin.demo.chapter1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
@@ -12,6 +15,14 @@ public class Chapter1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+		return "hi " + name + " ,i am from port:" + port;
 	}
 
 }
