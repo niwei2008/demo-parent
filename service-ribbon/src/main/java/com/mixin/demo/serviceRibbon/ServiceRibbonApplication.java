bc93759b4989e6b3112bc67c793e8783f3d12b6f
niweiMac <niwei@yodoo.net.cn>
2019-12-17 15:14:26 +0800

    ribbon + hystrix

--- service-ribbon/src/main/java/com/mixin/demo/serviceRibbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/mixin/demo/serviceRibbon/ServiceRibbonApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
