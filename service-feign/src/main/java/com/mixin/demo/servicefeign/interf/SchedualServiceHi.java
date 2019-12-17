586215e248d1328501f86b226231470e4636d83d
niweiMac <niwei@yodoo.net.cn>
2019-12-17 15:18:50 +0800

    feign + hystrix

--- service-feign/src/main/java/com/mixin/demo/servicefeign/interf/SchedualServiceHi.java
+++ service-feign/src/main/java/com/mixin/demo/servicefeign/interf/SchedualServiceHi.java
@@ -3,6 +3,7 @@
 package com.mixin.demo.servicefeign.interf;
 
 
+import com.mixin.demo.servicefeign.service.SchedualServiceHiHystric;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
---
+++ service-feign/src/main/java/com/mixin/demo/servicefeign/service/SchedualServiceHiHystric.java
@@ -0,0 +1,13 @@
+package com.mixin.demo.servicefeign.service;
+
+import com.mixin.demo.servicefeign.interf.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
+
