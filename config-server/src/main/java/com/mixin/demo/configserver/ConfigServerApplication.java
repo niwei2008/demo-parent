303b32eb248b594f500e218f725172e7e358d51b
niweiMac <niwei@yodoo.net.cn>
2019-12-17 16:32:08 +0800

    config server and config client link use eureka

--- config-server/src/main/java/com/mixin/demo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/mixin/demo/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.mixin.demo.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 // http://localhost:8888/config-client/dev/master
 // http://localhost:8888/master/config-client-dev.properties
 public class ConfigServerApplication {
