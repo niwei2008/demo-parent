d62cd8ac1beda735d09b53ff88b51d35ff9d863e
niweiMac <niwei@yodoo.net.cn>
2019-12-19 11:21:20 +0800

    add ssm

---
+++ ssm/src/main/java/com/mixin/demo/ssm/SsmApplication.java
@@ -0,0 +1,66 @@
+package com.mixin.demo.ssm;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class SsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+		return "hi " + name + " ,i am from port:" + port;
+	}
+
+
+	//zipkin start
+	private static final Logger LOG = Logger.getLogger(SsmApplication.class.getName());
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/hi2")
+	public String callHome(){
+		LOG.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+	@RequestMapping("/info")
+	public String info(){
+		LOG.log(Level.INFO, "calling trace service-hi ");
+
+		return "i'm service-hi";
+
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+
+}
