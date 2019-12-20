c0baab07a517e3a4d4c35dc8e757d9ddf099042e
niweiMac <niwei@yodoo.net.cn>
2019-12-20 15:57:06 +0800

    mybatis 1

---
+++ ssm/src/main/java/com/mixin/demo/ssm/mybatis/UsersMapper.java
@@ -0,0 +1,13 @@
+package com.mixin.demo.ssm.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+
+@Mapper
+public interface UsersMapper {
+   /* @Select("select * from USERS")
+    @Results()
+    list<Users> getAll();
+
+    */
+}
