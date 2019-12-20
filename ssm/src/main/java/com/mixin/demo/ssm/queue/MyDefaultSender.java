6aa6830b098a3e4dbaef9544795e9af1b28775f4
niweiMac <niwei@yodoo.net.cn>
2019-12-20 16:18:41 +0800

    logback debug level

--- ssm/src/main/java/com/mixin/demo/ssm/queue/MyDefaultSender.java
+++ ssm/src/main/java/com/mixin/demo/ssm/queue/MyDefaultSender.java
@@ -1,17 +1,24 @@
 package com.mixin.demo.ssm.queue;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyDefaultSender {
+    private final static Logger logger = LoggerFactory.getLogger(MyDefaultSender.class);
+
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public void send() {
         //(队列,消息内容)
         System.out.println("rabbitTemplate:"+rabbitTemplate);
+        logger.info("info log");
+        logger.debug("debug log");
+        logger.error("error log");
         rabbitTemplate.convertAndSend("myDefaultQueue","this is a message");
     }
 }
